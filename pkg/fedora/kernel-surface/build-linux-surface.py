773f65f4c073fdface008ff5d458c76eddbf55fb
Anthony Lee <108008609+orthogonaleety@users.noreply.github.com>
2026-07-21 05:24:36 +0100

    Update PACKAGE_TAG to kernel-7.1.4-1

--- pkg/fedora/kernel-surface/build-linux-surface.py
+++ pkg/fedora/kernel-surface/build-linux-surface.py
@@ -19,7 +19,7 @@ PACKAGE_NAME = "fedsurface"
 ## Fedora tags: kernel-X.Y.Z
 ## Upstream tags: vX.Y.Z
 ##
-PACKAGE_TAG = "kernel-7.1.3-1"
+PACKAGE_TAG = "kernel-7.1.4-1"
 
 ##
 ## The release number of the modified kernel package.
