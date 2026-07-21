84f8f96e39f42098dbbbd78ebd40931c3f5b60c5
Vaughan Milliman <vaughan@vaughanm.xyz>
2026-07-21 14:22:43 -0500

    fixed tag

--- pkg/fedora/kernel-surface/build-linux-surface.py
+++ pkg/fedora/kernel-surface/build-linux-surface.py
@@ -19,7 +19,7 @@ PACKAGE_NAME = "surface"
 ## Fedora tags: kernel-X.Y.Z
 ## Upstream tags: vX.Y.Z
 ##
-PACKAGE_TAG = "kernel-7.1.4"
+PACKAGE_TAG = "kernel-7.1.4-2"
 
 ##
 ## The release number of the modified kernel package.
